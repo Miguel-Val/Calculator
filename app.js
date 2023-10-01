9e7045993b085e50f0f79a1d65c2940cfb7e1b43
Miguel-Val <chahpo@gmail.com>
2023-09-30 22:52:27 -0400

    Fixed labels, added styling, output overflow n/w

--- app.js
+++ app.js
@@ -1,171 +1,115 @@
-const clearBtn = document.querySelector('.clearBtn');
-const posnegBtn = document.querySelector('.posnegBtn');
-const percentBtn = document.querySelector('.percentBtn');
-const divideBtn = document.querySelector('.divideBtn');
-const sevenBtn = document.querySelector('.sevenBtn');
-const eightBtn = document.querySelector('.eightBtn');
-const nineBtn = document.querySelector('.nineBtn');
-const timesBtn = document.querySelector('.timesBtn');
-const fourBtn = document.querySelector('.fourBtn');
-const fiveBtn = document.querySelector('.fiveBtn');
-const sixBtn = document.querySelector('.sixBtn');
-const subtractBtn = document.querySelector('.subtractBtn');
-const oneBtn = document.querySelector('.oneBtn');
-const twoBtn = document.querySelector('.twoBtn');
-const threeBtn = document.querySelector('.threeBtn');
-const addBtn = document.querySelector('.addBtn');
-const zeroBtn = document.querySelector('.zeroBtn');
-const decimalBtn = document.querySelector('.decimalBtn');
-const equalsBtn = document.querySelector('.equalsBtn');
 const output = document.querySelector('.output');
-const answer = document.querySelector('.answer');
-
-
-let outputText = '';
-let firstNumber = ''; 
+let firstNumber = '';
 let operator = '';
-let secondNumber = ''; 
+let secondNumber = '';
 let isEnteringSecondNumber = false;
+let decimalPressed = false;
 
-
-clearBtn.addEventListener('click', () => {
-  firstNumber = ''
-  secondNumber = ''
-  operator = ''
-  outputText = ''
-  output.innerHTML = '';
-});
-
-posnegBtn.addEventListener('click', () => {
-  
-});
-percentBtn.addEventListener('click', () => {
-  
-});
-
-divideBtn.addEventListener('click', () => {
-  handleOperatorClick('÷');
-  handleNumberClick('', '÷');
-});
-
-addBtn.addEventListener('click', () => {
-  handleOperatorClick('+')
-  handleNumberClick('', '+');
-  
-});
-
-subtractBtn.addEventListener('click', () => {
-  handleOperatorClick('-');
-  handleNumberClick('', '-');
-});
-
-timesBtn.addEventListener('click', () => {
-  handleOperatorClick('x');
-  handleNumberClick('', 'x');
-});
-
-
-zeroBtn.addEventListener('click', () => {
-  handleNumberClick('0', operator);
-});
-
-oneBtn.addEventListener('click', () => {
-  handleNumberClick('1', operator);
-});
-twoBtn.addEventListener('click', () => {
-  handleNumberClick('2', operator);
-});
-threeBtn.addEventListener('click', () => {
-  handleNumberClick('3', operator);
-});
-fourBtn.addEventListener('click', () => {
-  handleNumberClick('4', operator);
-});
-fiveBtn.addEventListener('click', () => {
-  handleNumberClick('5', operator);
-});
-sixBtn.addEventListener('click', () => {
-  handleNumberClick('6', operator);
-});
-sevenBtn.addEventListener('click', () => {
-  handleNumberClick('7', operator);
-});
-eightBtn.addEventListener('click', () => {
-  handleNumberClick('8', operator);
-});
-nineBtn.addEventListener('click', () => {
-  handleNumberClick('9', operator);
-});
-
-equalsBtn.addEventListener('click', () => {
- operate()
-  
-});
-
-
-function handleNumberClick(digit) {
-  if (!isEnteringSecondNumber) {
-    firstNumber += digit;
-    outputText += digit;
-  } else {
-    secondNumber += digit;
-    outputText += digit;
-  }
-  output.innerHTML = outputText;
+function clearCalculator() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    output.textContent = '0';
 }
 
-function handleOperatorClick(op) { 
-  if (firstNumber !== '') {
-    if (operator !== ''){
-      operate();
+function handleOperatorClick(op) {
+    if (firstNumber !== '') {
+        if (operator !== '') {
+            operate();
+        }
+        operator = op;
+        isEnteringSecondNumber = true;
+        output.textContent += ` ${op} `;
+        decimalPressed = false;
     }
-    operator = op;
-    isEnteringSecondNumber = true; 
-    outputText += ' ' + op + ' ';
-    output.innerHTML = outputText;
-  }
+}
 
+function handleNumberClick(digit) {
+    if (!isEnteringSecondNumber) {
+        if (firstNumber.length < 9) {
+            firstNumber += digit;
+            output.textContent = firstNumber;
+        }
+    } else {
+        if (secondNumber.length < 9) {
+            secondNumber += digit;
+            output.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+        }
+    }
 }
 
 function operate() {
-  if(firstNumber !== '' && secondNumber !== '') {
-    switch (operator) {
-      case '+':
-        firstNumber = (parseFloat(firstNumber) + parseFloat(secondNumber)).toString();
-        break;
-      case '-':
-        firstNumber = (parseFloat(firstNumber) - parseFloat(secondNumber)).toString();
-        break;
-      case 'x':
-        firstNumber = (parseFloat(firstNumber) * parseFloat(secondNumber)).toString();
-        break;
-        case '÷':
-          if (parseFloat(secondNumber) === 0) {
-            alert("Division by zero is not allowed.");
-            return;
-          }
-          firstNumber = (parseFloat(firstNumber) / parseFloat(secondNumber)).toString();
-          break;
+    if (firstNumber !== '' && secondNumber !== '') {
+        switch (operator) {
+            case '+':
+                firstNumber = (parseFloat(firstNumber) + parseFloat(secondNumber)).toString();
+                break;
+            case '-':
+                firstNumber = (parseFloat(firstNumber) - parseFloat(secondNumber)).toString();
+                break;
+            case 'x':
+                firstNumber = (parseFloat(firstNumber) * parseFloat(secondNumber)).toString();
+                break;
+            case '÷':
+                if (parseFloat(secondNumber) === 0) {
+                    alert("Division by zero is not allowed.");
+                    clearCalculator();
+                    return;
+                }
+                let result = parseFloat(firstNumber) / parseFloat(secondNumber);
+                firstNumber = result.toFixed(2);
+                break;
+        }
+        output.textContent = firstNumber;
+        secondNumber = '';
+        isEnteringSecondNumber = false;
+        operator = '';
     }
-    outputText = firstNumber;
-    secondNumber = '';
-    isEnteringSecondNumber = false;
-    operator = '';
-    output.innerHTML = outputText;
-  }
-    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Event listeners for button clicks
+document.querySelector('.clearBtn').addEventListener('click', clearCalculator);
+document.querySelector('.divideBtn').addEventListener('click', () => handleOperatorClick('÷'));
+document.querySelector('.addBtn').addEventListener('click', () => handleOperatorClick('+'));
+document.querySelector('.subtractBtn').addEventListener('click', () => handleOperatorClick('-'));
+document.querySelector('.timesBtn').addEventListener('click', () => handleOperatorClick('x'));
+document.querySelector('.zeroBtn').addEventListener('click', () => handleNumberClick('0'));
+document.querySelector('.oneBtn').addEventListener('click', () => handleNumberClick('1'));
+document.querySelector('.twoBtn').addEventListener('click', () => handleNumberClick('2'));
+document.querySelector('.threeBtn').addEventListener('click', () => handleNumberClick('3'));
+document.querySelector('.fourBtn').addEventListener('click', () => handleNumberClick('4'));
+document.querySelector('.fiveBtn').addEventListener('click', () => handleNumberClick('5'));
+document.querySelector('.sixBtn').addEventListener('click', () => handleNumberClick('6'));
+document.querySelector('.sevenBtn').addEventListener('click', () => handleNumberClick('7'));
+document.querySelector('.eightBtn').addEventListener('click', () => handleNumberClick('8'));
+document.querySelector('.nineBtn').addEventListener('click', () => handleNumberClick('9'));
+document.querySelector('.equalsBtn').addEventListener('click', operate);
+document.querySelector('.percentBtn').addEventListener('click', () => {
+    if (firstNumber !== '' && !isEnteringSecondNumber) {
+        firstNumber = (parseFloat(firstNumber) / 100).toString();
+        output.textContent = firstNumber;
+    } else if (secondNumber !== '' && isEnteringSecondNumber) {
+        secondNumber = (parseFloat(secondNumber) / 100).toString();
+        output.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    }
+});
+document.querySelector('.posnegBtn').addEventListener('click', () => {
+    if (firstNumber !== '' && !isEnteringSecondNumber) {
+        firstNumber = (parseFloat(firstNumber) * -1).toString();
+        output.textContent = firstNumber;
+    } else if (secondNumber !== '' && isEnteringSecondNumber) {
+        secondNumber = (parseFloat(secondNumber) * -1).toString();
+        output.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    }
+});
+document.querySelector('.decimalBtn').addEventListener('click', () => {
+    if (!decimalPressed) {
+        if (!isEnteringSecondNumber) {
+            firstNumber += '.';
+        } else {
+            secondNumber += '.';
+        }
+        output.textContent += '.';
+        decimalPressed = true;
+    }
+});
